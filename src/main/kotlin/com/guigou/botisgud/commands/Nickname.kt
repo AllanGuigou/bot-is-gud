9d09b454c18b1fe9758c5a12fab473e0b9634f62
Allan Guigou <allan.guigou@gmail.com>
2020-11-25 14:39:00 -0500

    Add logger to nickname command

--- src/main/kotlin/com/guigou/botisgud/commands/Nickname.kt
+++ src/main/kotlin/com/guigou/botisgud/commands/Nickname.kt
@@ -3,9 +3,11 @@ package com.guigou.botisgud.commands
 import com.gitlab.kordlib.common.entity.Snowflake
 import com.gitlab.kordlib.core.Kord
 import com.gitlab.kordlib.core.behavior.edit
+import com.guigou.botisgud.extensions.logger
 import com.guigou.botisgud.models.AbsoluteReminderTrigger
 import com.guigou.botisgud.services.WordService
 import com.guigou.botisgud.services.WordServiceImpl
+import io.ktor.util.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -19,27 +21,36 @@ class Nickname(
     private val options: NicknameOptions = NicknameOptions(),
     private val wordService: WordService = WordServiceImpl(),
 ) : Command {
+    companion object {
+        val logger = logger()
+    }
 
     override suspend fun register(client: Kord, scope: CoroutineScope) {
-        val trigger = AbsoluteReminderTrigger(options.commandTriggerExpression)
+        logger.info("register nickname command")
 
+        val trigger = AbsoluteReminderTrigger(options.commandTriggerExpression)
         scope.launch(Dispatchers.Default) {
             while (true) {
-                delay(trigger.timestamp().toEpochMilli() - now().toEpochMilli())
+                val delayTill = trigger.timestamp()
+                logger.info("delay processing till [${delayTill}]")
+                delay(delayTill.toEpochMilli() - now().toEpochMilli())
+                logger.info("begin processing")
 
                 for (entry in users) {
                     for (userSnowflake in entry.value) {
-                        val member = client.getUser(userSnowflake)!!.asMember(entry.key)
-
                         try {
+                            val member = client.getUser(userSnowflake)!!.asMember(entry.key)
+                            logger.trace("processing [${member.displayName}]")
                             member.edit {
                                 nickname = wordService.random()
                             }
                         } catch (ex: Exception) {
-                            // TODO: log exception
+                            logger.error(ex)
                         }
                     }
                 }
+
+                logger.info("processing completed")
             }
         }
     }
