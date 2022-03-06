e864d86f3391256ed1beff620677ce6cd336275e
Allan Guigou <allan.guigou@gmail.com>
2022-03-06 15:14:39 -0500

    feat: setup go-is-gud
    I'm not enjoying working with kotlin, tired of dealing with the cruft
    from java. I've been interested in golang and thought this would be a
    good time to give it a try.
    
    Setup the core functionality that bot-is-gud is known for, typing!

---
+++ go-is-gud/README.md
@@ -0,0 +1,14 @@
+# go-is-gud
+
+## Build
+
+```bash
+go build -o bin/bot-is-gud
+```
+
+## Usage
+
+```bash
+./bin/bot-is-gud -t DISCORD_TOKEN
+```
+
---
+++ go-is-gud/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+	dg.AddHandler(typingStart)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	dg.Identify.Intents = discordgo.IntentsGuildMessageTyping
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+	fmt.Println()
+}
+
+// not thread safe but no big deal if this triggers twice
+var LastTypedAt time.Time = time.Unix(0, 0)
+
+func triggerTyping(s *discordgo.Session, cid string) {
+	if LastTypedAt.Add(time.Minute).After(time.Now().UTC()) {
+		fmt.Println("typing too soon")
+		return
+	}
+
+	LastTypedAt = time.Now().UTC()
+	err := s.ChannelTyping(cid)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("typing triggered")
+}
+
+func typingStart(s *discordgo.Session, m *discordgo.TypingStart) {
+	// ignore all messages created by the bot itself
+	if m.UserID == s.State.User.ID {
+		return
+	}
+
+	triggerTyping(s, m.ChannelID)
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	triggerTyping(s, m.ChannelID)
+}
