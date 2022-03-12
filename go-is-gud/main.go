830c504dff51aadbbe3c4dae5bb501d3d5ac76af
Allan Guigou <allan.guigou@gmail.com>
2022-03-12 18:25:02 -0500

    feat: setup a /bigly discord slash command
    /bigly will return a random word from a predefined list.

--- go-is-gud/main.go
+++ go-is-gud/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"guigou/bot-is-gud/health"
+	"math/rand"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
-
-	"guigou/bot-is-gud/health"
 )
 
 var (
@@ -44,8 +44,15 @@ func main() {
 
 	dg.AddHandler(messageCreate)
 	dg.AddHandler(typingStart)
+	dg.AddHandler(slashCommandHandler)
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 	dg.Identify.Intents = discordgo.IntentsGuildMessageTyping
+
+	command := &discordgo.ApplicationCommand{
+		Name:        "bigly",
+		Type:        discordgo.ChatApplicationCommand,
+		Description: "Word of the day!",
+	}
 
 	err = dg.Open()
 	if err != nil {
@@ -53,12 +60,56 @@ func main() {
 		return
 	}
 
+	dg.ApplicationCommandCreate(dg.State.User.ID, "", command)
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
 	dg.Close()
 	fmt.Println()
+}
+
+// rand.Seed(time.Now().UnixNano())
+var wl []string = []string{
+	"aback",
+	"abase",
+	"abate",
+	"abbey",
+	"abbot",
+	"abhor",
+	"abide",
+	"abled",
+	"abode",
+	"abort",
+}
+
+func rw() string {
+	i := rand.Intn(len(wl))
+	return wl[i]
+}
+
+func slashCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.Type != discordgo.InteractionApplicationCommand {
+		return
+	}
+
+	switch command := i.ApplicationCommandData().Name; command {
+	case "bigly":
+		{
+			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: rw(),
+					Flags:   uint64(discordgo.MessageFlagsEphemeral),
+				},
+			})
+
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
 }
 
 func triggerTyping(s *discordgo.Session, cid string) {
