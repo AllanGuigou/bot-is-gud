9e2dea4b6d3f0c8c8d86e17170e68423494bf7e0
Allan Guigou <allan.guigou@gmail.com>
2022-07-05 11:08:33 -0400

    fix: log guild when slash commands are modified
    Can DM bot-is-gud to enable/disable global slash commands since guildId
    will be an empty string.

--- go-is-gud/slash.go
+++ go-is-gud/slash.go
@@ -37,7 +37,7 @@ func (s *Slash) add(name, description, guildId string) {
 	s.dg.ApplicationCommandCreate(s.dg.State.User.ID, guildId, ac)
 	s.commands[name] = ac
 
-	log.Println(fmt.Sprintf("Added %s slash command", name))
+	log.Println(fmt.Sprintf("Added %s slash command to guild '%s'", name, guildId))
 }
 
 func (s *Slash) remove(name, guildId string) {
@@ -53,5 +53,5 @@ func (s *Slash) remove(name, guildId string) {
 	// bulk overwrite seems to cause commands to be removed immediately
 	s.dg.ApplicationCommandBulkOverwrite(s.dg.State.User.ID, guildId, commands)
 	delete(s.commands, name)
-	log.Println(fmt.Sprintf("Removed %s slash command", name))
+	log.Println(fmt.Sprintf("Removed %s slash command from guild '%s'", name, guildId))
 }
