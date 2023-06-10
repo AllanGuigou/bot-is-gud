9072e20d2c5f5b837e8059125602350f7b45c45b
AllanGuigou <allan.guigou@gmail.com>
2023-06-10 09:59:56 -0400

    Refactor slash command handlers to Slash

--- go-is-gud/main.go
+++ go-is-gud/main.go
@@ -15,7 +15,6 @@ import (
 	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"go.uber.org/ratelimit"
 )
 
 func init() {
@@ -28,12 +27,6 @@ type Event struct {
 	timestamp time.Time
 	user      string
 	action    string
-}
-
-func track(c <-chan Event) {
-	for event := range c {
-		fmt.Printf("%s %s %s\n", event.timestamp, event.user, event.action)
-	}
 }
 
 var slash *Slash
@@ -49,8 +42,6 @@ func main() {
 	}
 
 	dg.AddHandler(typingStart)
-	c := make(chan Event, 100)
-	go track(c)
 	dg.Identify.Intents =
 		discordgo.IntentsMessageContent +
 			discordgo.IntentsDirectMessages +
@@ -76,13 +67,11 @@ func main() {
 		p := New(ctx, db)
 		api.RegisterHealthCheck(func() bool { return p.IsHealthy() })
 		dg.AddHandler(messageCreate(p))
-		dg.AddHandler(slashCommandHandler(c, p))
+		slash = NewSlash(dg, p)
 	} else {
 		dg.AddHandler(messageCreate(nil))
-		dg.AddHandler(slashCommandHandler(c, nil))
+		slash = NewSlash(dg, nil)
 	}
-
-	slash = NewSlash(dg)
 
 	if env.ENABLE_BIGLY {
 		dg.ApplicationCommandCreate(dg.State.User.ID, "", &discordgo.ApplicationCommand{
@@ -100,176 +89,6 @@ func main() {
 
 	dg.Close()
 	fmt.Println()
-}
-
-func eventFromInteraction(i discordgo.InteractionCreate) Event {
-	var user string
-	if i.Member != nil {
-		user = i.Member.User.ID
-	} else if i.User != nil {
-		user = i.User.ID
-	}
-
-	// TODO: how to get timestamp from interaction
-	return Event{timestamp: time.Now(), user: user, action: "bigly-slash-command"}
-}
-
-func slashCommandHandler(c chan<- Event, p *Presence) func(*discordgo.Session, *discordgo.InteractionCreate) {
-	rl := ratelimit.New(5, ratelimit.Per(1*time.Minute))
-	return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		switch i.Type {
-		case discordgo.InteractionApplicationCommand:
-			{
-				command := i.ApplicationCommandData()
-				switch cn := command.Name; cn {
-				case "me":
-					{
-						event := eventFromInteraction(*i)
-						c <- event
-						err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-							Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
-							Data: &discordgo.InteractionResponseData{
-								Flags: uint64(discordgo.MessageFlagsEphemeral),
-							},
-						})
-
-						if err != nil {
-							fmt.Println(err)
-							return
-						}
-
-						rl.Take()
-						var content string
-						// TODO: use dg state to provide more accurate results if the user recently connected or disconnected
-						presence := p.GetUser(event.user)
-						if presence == nil {
-							content = "Oops something went wrong..."
-							_, err = s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-								Content: content,
-							})
-
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
-						} else if presence.HasPresence {
-							content = fmt.Sprintf("You've been active for %s", strings.ReplaceAll(presence.Duration.String(), "0s", ""))
-							_, err = s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-								Content: content,
-							})
-
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
-						} else {
-							content = "You've been inactive"
-							_, err = s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-								Content: content,
-							})
-
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
-						}
-						fmt.Printf("'/me' slash command used for user: '%s' content: '%s'\n", event.user, content)
-					}
-				case "profile":
-					{
-						c <- eventFromInteraction(*i)
-						err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-							Type: discordgo.InteractionResponseModal,
-							Data: &discordgo.InteractionResponseData{
-								CustomID: "profile_" + i.Interaction.Member.User.ID,
-								Title:    "Profile",
-								Components: []discordgo.MessageComponent{
-									discordgo.ActionsRow{
-										Components: []discordgo.MessageComponent{
-											discordgo.TextInput{
-												CustomID: "birthday_month",
-												Label:    "Month",
-												Style:    discordgo.TextInputShort,
-												Required: true,
-												Value:    "07",
-											},
-										},
-									},
-									discordgo.ActionsRow{
-										Components: []discordgo.MessageComponent{
-											discordgo.TextInput{
-												CustomID: "birthday_day",
-												Label:    "Day",
-												Style:    discordgo.TextInputShort,
-												Required: true,
-												Value:    "24",
-											},
-										},
-									},
-								},
-							}},
-						)
-
-						if err != nil {
-							fmt.Println(err)
-						}
-					}
-				case "lets-gamble":
-					{
-						err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-							Type: discordgo.InteractionResponseChannelMessageWithSource,
-							Data: &discordgo.InteractionResponseData{
-								Content: "You've lost.",
-								Flags:   uint64(discordgo.MessageFlagsEphemeral),
-							},
-						},
-						)
-
-						if err != nil {
-							fmt.Println(err)
-							return
-						}
-
-						t := time.Now().Add(time.Second * time.Duration(rand.Intn(1000)))
-
-						if i.Member == nil || i.Member.User == nil {
-							fmt.Println("Attempt to `lets-gamble` from outside of a guild.")
-							return
-						}
-
-						err = s.GuildMemberTimeout(i.GuildID, i.Member.User.ID, &t)
-
-						if err != nil {
-							fmt.Println(err)
-							return
-						}
-
-					}
-				}
-			}
-		case discordgo.InteractionModalSubmit:
-			{
-				err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Flags: 1 << 6,
-					},
-				})
-
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				p, err := NewProfile(i.ModalSubmitData())
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				fmt.Println(p)
-			}
-		}
-	}
 }
 
 func triggerTyping(s *discordgo.Session, cid string) {
--- go-is-gud/slash.go
+++ go-is-gud/slash.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"go.uber.org/ratelimit"
 )
 
 type Slash struct {
 	dg       *discordgo.Session
 	mu       sync.Mutex
 	commands map[string]*discordgo.ApplicationCommand
+
+	presence *Presence
 }
 
-func NewSlash(dg *discordgo.Session) *Slash {
+func NewSlash(dg *discordgo.Session, p *Presence) *Slash {
 	// TODO: how to keep state of all commands at initialization
 	s := Slash{
 		dg:       dg,
 		mu:       sync.Mutex{},
 		commands: make(map[string]*discordgo.ApplicationCommand),
+		presence: p,
 	}
+
+	dg.AddHandler(s.commandHandler)
 
 	return &s
 }
@@ -61,4 +70,183 @@ func (s *Slash) remove(name, guildID string) {
 
 	delete(s.commands, name)
 	fmt.Printf("Removed %s slash command from guild '%s'\n", name, guildID)
+}
+
+func eventFromInteraction(i discordgo.InteractionCreate) Event {
+	var user string
+	if i.Member != nil {
+		user = i.Member.User.ID
+	} else if i.User != nil {
+		user = i.User.ID
+	}
+
+	// TODO: how to get timestamp from interaction
+	return Event{timestamp: time.Now(), user: user, action: "bigly-slash-command"}
+}
+
+func (slash *Slash) commandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	ratelimitMe := ratelimit.New(5, ratelimit.Per(1*time.Minute))
+	switch i.Type {
+	case discordgo.InteractionApplicationCommand:
+		{
+			command := i.ApplicationCommandData()
+			event := eventFromInteraction(*i)
+			fmt.Printf("%s %s %s\n", event.timestamp, event.user, event.action)
+			switch cn := command.Name; cn {
+			case "me":
+				{
+					meCommand(s, i.Interaction, event, ratelimitMe, slash.presence)
+				}
+			case "profile":
+				{
+					profileCommand(s, i.Interaction)
+				}
+			case "lets-gamble":
+				{
+					gambleCommand(s, i.Interaction)
+				}
+			}
+		}
+	case discordgo.InteractionModalSubmit:
+		{
+			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Flags: 1 << 6,
+				},
+			})
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			p, err := NewProfile(i.ModalSubmitData())
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Println(p)
+		}
+	}
+}
+
+func meCommand(s *discordgo.Session, i *discordgo.Interaction, event Event, ratelimit ratelimit.Limiter, presence *Presence) {
+	err := s.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Flags: uint64(discordgo.MessageFlagsEphemeral),
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ratelimit.Take()
+	var content string
+	// TODO: use dg state to provide more accurate results if the user recently connected or disconnected
+	up := presence.GetUser(event.user)
+	if up == nil {
+		content = "Oops something went wrong..."
+		_, err = s.FollowupMessageCreate(i, true, &discordgo.WebhookParams{
+			Content: content,
+		})
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else if up.HasPresence {
+		content = fmt.Sprintf("You've been active for %s", strings.ReplaceAll(up.Duration.String(), "0s", ""))
+		_, err = s.FollowupMessageCreate(i, true, &discordgo.WebhookParams{
+			Content: content,
+		})
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else {
+		content = "You've been inactive"
+		_, err = s.FollowupMessageCreate(i, true, &discordgo.WebhookParams{
+			Content: content,
+		})
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	fmt.Printf("'/me' slash command used for user: '%s' content: '%s'\n", event.user, content)
+}
+
+func profileCommand(s *discordgo.Session, i *discordgo.Interaction) {
+	err := s.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseModal,
+		Data: &discordgo.InteractionResponseData{
+			CustomID: "profile_" + i.Member.User.ID,
+			Title:    "Profile",
+			Components: []discordgo.MessageComponent{
+				discordgo.ActionsRow{
+					Components: []discordgo.MessageComponent{
+						discordgo.TextInput{
+							CustomID: "birthday_month",
+							Label:    "Month",
+							Style:    discordgo.TextInputShort,
+							Required: true,
+							Value:    "07",
+						},
+					},
+				},
+				discordgo.ActionsRow{
+					Components: []discordgo.MessageComponent{
+						discordgo.TextInput{
+							CustomID: "birthday_day",
+							Label:    "Day",
+							Style:    discordgo.TextInputShort,
+							Required: true,
+							Value:    "24",
+						},
+					},
+				},
+			},
+		}},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func gambleCommand(s *discordgo.Session, i *discordgo.Interaction) {
+	err := s.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "You've lost.",
+			Flags:   uint64(discordgo.MessageFlagsEphemeral),
+		},
+	},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	t := time.Now().Add(time.Second * time.Duration(rand.Intn(1000)))
+
+	if i.Member == nil || i.Member.User == nil {
+		fmt.Println("Attempt to `lets-gamble` from outside of a guild.")
+		return
+	}
+
+	err = s.GuildMemberTimeout(i.GuildID, i.Member.User.ID, &t)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
